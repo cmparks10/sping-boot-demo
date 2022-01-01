02c6f6d3c9ec89e2fd02649fe28226a4da73f9ca
Corey <cmparks10@gmail.com>
2022-01-01 16:07:52 -0500

    Got a working http call to the pokeapi! We need to seperate the header logic to its own method and decouple the code in the controller next

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jdk.nashorn.internal.objects.NativeJSON;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestOperations;
@@ -17,6 +19,7 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.Exchanger;
 
@@ -28,42 +31,39 @@ public class HelloController {
         return "Greetings from Spring Boot!";
     }
 
-    @GetMapping("/testURL")
-    public void testURL() throws IOException {
-        final String uri = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
-        URL url = new URL("http://localhost:8080/foobar");
-        URLConnection hc = url.openConnection();
-        hc.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+    @GetMapping(value = "/pokemonNotWorking")
+    public static void pokemonNotWorking(@RequestHeader(value="User-Agent") String userAgent) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            final String uri = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
+            String result = restTemplate.getForObject(uri, String.class);
+            System.out.println(result);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    };
 
-        System.out.println("Here is the connection: "+hc.getContentType());
-        //returns text/html;charset=UTF-8
+    @GetMapping(value = "/pokemon")
+    public static ResponseEntity<String> Pokemon() {
+        ResponseEntity<String> responseJson = null;
+        try {
+            final String resourceUrl = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
+            RestTemplate restTemplate = new RestTemplate();
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+            responseJson = restTemplate.exchange(resourceUrl, HttpMethod.GET, entity, String.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return responseJson;
+    };
+
+    public void setHeaders(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("user-agent", "Application");
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
     }
-
-    @GetMapping("/pokemon")
-    public JSONObject pokeIndex() throws IOException {
-        //final String uri = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
-        final String uri = "https://gorest.co.in/public/v1/users";
-
-        //JSONObject json = new JSONObject();
-        //System.out.println("1");
-        RestTemplate restTemplate = new RestTemplate();
-        System.out.println("here");
-        JSONObject result = restTemplate.getForObject(uri, JSONObject.class);
-        //String resultt = restTemplate.getForObject(uri, String.class);
-        System.out.println("3");
-        System.out.println(result);
-        return result;
-    }
-
-
-    @GetMapping("/GoRestApiV1Users")
-    public String getFromUrl2() throws JsonProcessingException {
-        final String uri = "https://gorest.co.in/public/v1/users";
-
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-        return result;
-    }
-
 
 }
